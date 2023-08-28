9cfe149be428f54cf940caf41a4d6600ce871611
Fagundes762 <rafaelfagundes762@gmail.com>
2023-08-28 19:54:49 -0300

    Adicionando movimento à nave, fundo, e inimigo

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 
 pygame.init()
@@ -23,13 +24,18 @@ naveInimiga = pygame.transform.scale(naveInimiga, (70, 70))
 position_player_x = 200
 position_player_y = 300
 
-position_inimigo_x = 900
+position_inimigo_x = 1280
 position_inimigo_y = 360
 
 
 
 
 rodando = True #Se tiver como 'True' está funcionando
+
+def respaw():
+    x = 1350
+    y = random.randint(1, 640)
+    return [x, y]
 
 """ Loop para verificar se o jogo está rodando """
 while rodando: #RODANDO = TRUE
@@ -44,22 +50,28 @@ while rodando: #RODANDO = TRUE
 
     if relativo_x < 1280:
         screen.blit(bg, (relativo_x, 0))
-
-        x -= 1
-
         tecla = pygame.key.get_pressed()
-        if tecla[pygame.K_UP] and position_player_y > 1:
-            position_player_y -= 1
 
-        if tecla[pygame.K_DOWN] and position_player_y < 665:
-            position_player_y += 1
+    if tecla[pygame.K_UP] and position_player_y > 1:
+        position_player_y -= 1
 
-        if tecla[pygame.K_SPACE]:
-            triggered = True
-            velocidade_missil_x = 4
+    if tecla[pygame.K_DOWN] and position_player_y < 665:
+        position_player_y += 1
 
+    if tecla[pygame.K_SPACE]:
+        triggered = True
+        velocidade_missil_x = 1
+
+    if position_inimigo_x <= 10:
+        position_inimigo_x = respaw()[0]
+        position_inimigo_y = respaw()[1]
+
+    x -= 1
+    position_inimigo_x -= 0.75
+    print(position_inimigo_x)
     screen.blit(player, (position_player_x, position_player_y))
     screen.blit(naveInimiga, (position_inimigo_x, position_inimigo_y))
+
         
 
 
